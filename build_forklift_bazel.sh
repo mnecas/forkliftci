d1c3fa03779e949b85fe36162a0636ffddd2b6f9
Martin Nečas <necas.marty@gmail.com>
2022-09-21 14:31:54 +0200

    Test

--- build_forklift_bazel.sh
+++ build_forklift_bazel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Running $0"
 
-cd forklift
+pushd ${FORKLIFT_DIR:-forklift}
 
 export REGISTRY=localhost:5001
 export REGISTRY_TAG=latest
@@ -12,3 +12,5 @@ bazel run push-forklift-operator-bundle
 bazel run push-forklift-operator-index --action_env REGISTRY=${REGISTRY} --action_env REGISTRY_TAG=${REGISTRY_TAG} --action_env OPM_OPTS="--use-http"
 bazel run push-forklift-controller
 bazel run push-forklift-validation
+
+popd
